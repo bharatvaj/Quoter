8e935a2d05d055e325fe4a28f2af0fcef45055a7
Bharatvaj <bharatvajph@gmail.com>
2018-10-12 02:55:46 +0530

    Improved quotes fetching algo and improved UI

--- app/src/main/java/com/thing/quoter/MainActivity.kt
+++ app/src/main/java/com/thing/quoter/MainActivity.kt
@@ -8,19 +8,21 @@ import android.util.Log
 import android.view.View
 import com.thing.quoter.model.Quote
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var randomIndex = Random()
+
     fun getNewQuote(): Quote {
-        quotes.populate(100)
         return if (quotes.size == 0)
             Quote("No new quotes, enjoy your life", "Quoter dev")
-        else quotes.keyAt(0)
+        else quotes.keyAt(randomIndex.nextInt(quotes.size))
     }
 
     fun updateQuote(quote: Quote) {
         quoteTextView.text = quote.quote
-        speakerTextView.text = quote.speaker
+        speakerTextView.text = getString(R.string.speaker_format, quote.speaker)
     }
 
     private lateinit var quotes: FirestoreList<Quote>
@@ -32,13 +34,19 @@ class MainActivity : AppCompatActivity() {
         class onAdd : ObservableMap.OnMapChangedCallback<ObservableMap<Quote, String>, Quote, String>() {
             override fun onMapChanged(sender: ObservableMap<Quote, String>, key: Quote) {
                 updateQuote(key)
+                //endloading
+                //mutex end
             }
         } //TODO simplify
         quotes.addOnMapChangedCallback(onAdd())
+        //read quotes from online
+        quotes.populate(100)
         //at start
         updateQuote(getNewQuote())
         //then register click
         rootView.setOnClickListener {
+            //startloading
+            //mutex start
             updateQuote(getNewQuote())
         }
     }
